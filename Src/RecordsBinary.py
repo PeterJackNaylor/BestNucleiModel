2046fcee997703467b5432cd24041283ad2ec7f4
pnaylor <peter.naylor@mines-paristech.fr>
2018-10-31 16:45:31 +0100

    update

--- Src/RecordsBinary.py
+++ Src/RecordsBinary.py
@@ -6,8 +6,8 @@ import os
 import argparse
 from segmentation_net import create_tfrecord, compute_mean
 
-from datagen_object import DataGenNeeraj
-from segmentation_net import DataGenNeerajBin, ExampleUNetDatagen
+from datagen_object import DataGenNeerajBin
+from segmentation_net import ExampleUNetDatagen
 
 
 
